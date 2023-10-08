70aeccda4caf24c036627a507de9dee5e4d8d582
Ryan Tang <ryan.tang.clu@gmail.com>
2023-10-08 12:19:25 -0400

    initial vite and ts configuration setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
+
+/* Notes
+- import path from 'path' 
+use this to implement pathing option instead of regular relative/absolute pathing
+
+- resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  }
+added so that when we use @ it will know what pathing we mean, replace current
+directory name with src.
+*/
